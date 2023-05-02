f53ca0da5dc297836f5f81927abcad4c88c4568e
a21angelmb <email>
2023-05-02 18:52:44 +0200

    Finalizado. Falta añadir boton retorno.

---
+++ app/src/main/java/com/example/loginusuario/UI/elements/LoginErrorScreen.kt
@@ -0,0 +1,23 @@
+package com.example.loginusuario.UI.elements
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginErrorScreen(paddingValues: PaddingValues) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+    ) {
+        Text(text ="Contraseña y/o usuario incorrectos" )
+    }
+}
